64f3cc84a4a76ccbdafe93ae7240603e424ee22d
Ignacio Ureta <ignacio@dealsyte.com>
2019-07-21 18:38:37 -0400

    slides

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: ${props => (props.color ? props.color : "#cacaca")};
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container color={"#f46969"}>
+        <div>pancho</div>
+      </Container>
+      <Container color={"#f4f4f4"}>
+        <div>pancho</div>
+      </Container>
+      <Container color={"#91b5d6"}>
+        <div>pancho</div>
+      </Container>
+    </>
   );
 }
 
